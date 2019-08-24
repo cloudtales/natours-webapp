51d0b5659ff965a739b7123ddf579225e989a851
Stephan Schaefer <stephan@cloudtales.de>
2019-08-25 01:44:38 +0200

    add protection (helmet, xss,hpp and mongosanitize)

--- app.js
+++ app.js
@@ -1,21 +1,69 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// MIDDLEWARES
+// GLOBAL MIDDLEWARES
+
+//Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+// Limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: {
+    status: 'fail',
+    message: 'Too many requests from this IP, please try again in an hour.'
+  }
+});
+app.use('/api', limiter); // apply limit only to /api
 
-app.use(express.json()); // middleware
+// Body parser, reading data from the body into req.body
+app.use(
+  express.json({
+    limit: '10kb'
+  })
+);
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter polution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
--- controllers/authController.js
+++ controllers/authController.js
@@ -13,17 +13,27 @@ const signToken = id => {
     expiresIn: process.env.JWT_EXPIRES_IN
   });
 };
-/*
+
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRS_IN * 24 * 60 * 60 * 1000 //dodgy..... get the date from the token
+    ),
+    //    secure: true, // cookie wil only be send when we use https
+    httpOnly: true // cookie can NOT be accessed or modified by the browser in any way to prevent Cross-Site Scripting attacks
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
   res.status(statusCode).json({
     status: 'success',
-    token,
-    data: {
-      user
-    }
+    token
   });
-};*/
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -34,11 +44,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordChangedAt: req.body.passwordChangedAt
   });
 
-  const token = signToken(newUser.id);
-  res.status(201).json({
-    status: 'success',
-    token
-  });
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -57,11 +63,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   //3) If everything ok, send token to client
-  const token = signToken(user.id);
-  res.status(200).json({
-    status: 'success',
-    token
-  });
+  createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -194,14 +196,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   // 4) Log the user in, send JWT token
 
-  const token = signToken(user.id);
-  res.status(200).json({
-    status: 'success',
-    token,
-    data: {
-      user
-    }
-  });
+  createSendToken(user, 200, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -218,9 +213,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   await user.save(); // will trigger validation and encrypt password
 
   // 4) Log user in, send JWT
-  const token = signToken(user.id);
-  res.status(200).json({
-    status: 'success',
-    token
-  });
+  createSendToken(user, 200, res);
 });
